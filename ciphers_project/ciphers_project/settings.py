133581a8ee83365c90cab6d33270235d35d186f2
SnehaManohar23 <81314939+SnehaManohar23@users.noreply.github.com>
2024-02-29 23:45:58 -0500

    Update settings.py

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xq3)&79uxf1u0a)xz21e_^q(jn@9bpm2feuo*6xm1ffe48dpxh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ciphers-service-arsivak-1bwz.onrender.com']
+ALLOWED_HOSTS = ['ciphers-service-latest-4bip.onrender.com']
 
 
 # Application definition
